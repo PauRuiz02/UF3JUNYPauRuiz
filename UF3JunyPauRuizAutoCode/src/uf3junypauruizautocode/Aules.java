1b30a68d9ff72f22e7a824083a6f899e3d622a38
Pau Ruiz Martin <101583566+PauRuiz02@users.noreply.github.com>
2022-06-13 14:22:08 +0200

    Add files via upload

---
+++ UF3JunyPauRuizAutoCode/src/uf3junypauruizautocode/Aules.java
@@ -0,0 +1,16 @@
+package uf3junypauruizautocode;
+
+public class Aules {
+
+    private String nom;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    
+}
